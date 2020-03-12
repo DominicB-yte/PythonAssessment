8a89fa9532f0883fc0ec3ba59671a362b518747a
Cerulean-2824 <53456554+Cerulean-2824@users.noreply.github.com>
2020-03-12 14:30:34 +1300

    Further Optimization of code

--- Main.py
+++ Main.py
@@ -1,19 +1,18 @@
 from Encoder import CaesarEncrypt, CaesarDecrypt
-if __name__ == "__main__":
-    Cryption = int(input("1. Encrypt\n2. Decrypt\n: "))
+Cryption = int(input("1. Encrypt\n2. Decrypt\n: "))
 
-    if Cryption == 1:
-        Cipher = int(input("1. Encrypt in Caesar\n2. Encrypt in Polyalphabetical\n: "))
-        if Cipher == 1:
-            CaesarEncrypt()
-        elif Cipher == 2:
-            pass
-            # Encrypt in Polyalphabetical
+if Cryption == 1:
+    Cipher = int(input("1. Encrypt in Caesar\n2. Encrypt in Polyalphabetical\n: "))
+    if Cipher == 1:
+        CaesarEncrypt()
+    elif Cipher == 2:
+        pass
+        # Encrypt in Polyalphabetical
 
-    if Cryption == 2:
-        Decode = int(input("1. Decrypt Caesar\n2. Decrypt Polyalphabetical\n: "))
-        if Decode == 1:
-            CaesarDecrypt()
-        elif Decode == 2:
-            pass
-            # Decrypt in Polyalphabetical
+if Cryption == 2:
+    Decode = int(input("1. Decrypt Caesar\n2. Decrypt Polyalphabetical\n: "))
+    if Decode == 1:
+        CaesarDecrypt()
+    elif Decode == 2:
+        pass
+        # Decrypt in Polyalphabetical
